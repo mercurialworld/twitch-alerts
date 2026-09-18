b5771887b50a1685c1cafc6daf25fd7cacd71568
ポッチャマ <mercworld@pocha.moe>
2026-09-18 19:10:33 -0400

    feat: add redeem images

--- scripts/main.js
+++ scripts/main.js
@@ -41,8 +41,24 @@ function ChangeAlertText(sentenceStart, user, sentenceMid, amount, sentenceEnd) 
     }
 }
 
-function ChangeImage(newImage) {
+function SetFlexDirection(alertPos) {
+    document.getElementById("overlay").style.flexDirection = alertPos === "top" ? "column" : "column-reverse";
+
+    if (alertPos === "top") {
+        document.getElementById("redeem-image").style.bottom = "-1000px";
+    } else {
+        document.getElementById("redeem-image").style.top = "-1000px";
+        document.getElementById("redeem-image").style.transform = "rotateX(180deg)";
+    }
+
+}
+
+function ChangeAlertImage(newImage) {
     document.getElementById("alert-image").src = newImage;
+}
+
+function ChangeRedeemImage(newImage) {
+    document.getElementById("redeem-image").src = newImage;
 }
 
 function PlayAudio(audioFile) {
@@ -52,30 +68,42 @@ function PlayAudio(audioFile) {
 }
 
 function ShowAlert() {
-    const alertElement = document.getElementById("popup");
+    const alertElement = document.getElementById("alert-popup");
     alertElement.classList.add("fadeIn");
     alertElement.classList.remove("fadeOut");
 }
 
 function HideAlert() {
-    const alertElement = document.getElementById("popup");
+    const alertElement = document.getElementById("alert-popup");
     alertElement.classList.add("fadeOut");
     alertElement.classList.remove("fadeIn");
 }
 
-function StartSBotClient(host, port, auth) {
+function ShowRedeem(direction) {
+    const redeemImage = document.getElementById("redeem-image");
+    redeemImage.classList.add(direction === "top" ? "moveInTop" : "moveInBottom");
+    redeemImage.classList.remove(direction === "top" ? "moveOutTop" : "moveOutBottom");
+}
+
+function HideRedeem(direction) {
+    const redeemImage = document.getElementById("redeem-image");
+    redeemImage.classList.add(direction === "top" ? "moveOutTop" : "moveOutBottom");
+    redeemImage.classList.remove(direction === "top" ? "moveInTop" : "moveInBottom");
+}
+
+function StartSBotClient(host, port, auth, direction) {
     const client = new StreamerbotClient({
         host: host,
         port: port,
         password: auth,
         subscribe: {
-            "Twitch": ["Follow", "Cheer", "Sub", "ReSub", "GiftSub", "Raid"]
+            "Twitch": ["Follow", "Cheer", "Sub", "ReSub", "GiftSub", "Raid", "RewardRedemption"]
         }
     });
 
     client.on("Twitch.Follow", (_) => {
         ChangeAlertText(null, null, "Thank you for the follow!", null, null);
-        ChangeImage("static/img/pipari.png");
+        ChangeAlertImage("static/img/pipari.png");
         ShowAlert();
 
         setTimeout(() => {
@@ -85,7 +113,7 @@ function StartSBotClient(host, port, auth) {
 
     client.on("Twitch.Raid", (data) => {
         ChangeAlertText("Thank you", data.data.from_broadcaster_user_name, "for the raid with", data.data.viewers, "people!");
-        ChangeImage("static/img/pipspin.gif");
+        ChangeAlertImage("static/img/pipspin.gif");
         ShowAlert();
         PlayAudio("static/sound/catch-modern.mp3");
 
@@ -96,7 +124,7 @@ function StartSBotClient(host, port, auth) {
 
     client.on("Twitch.Sub", (data) => {
         ChangeAlertText("Thank you", data.data.user.name, "for the", `tier ${data.data.sub_tier / 1000}`, "subscription!");
-        ChangeImage("static/img/pipwaddle.gif");
+        ChangeAlertImage("static/img/pipwaddle.gif");
         ShowAlert();
         PlayAudio("static/sound/catch-classic.mp3");
 
@@ -107,7 +135,7 @@ function StartSBotClient(host, port, auth) {
 
     client.on("Twitch.ReSub", (data) => {
         ChangeAlertText("Thank you", data.data.user.name, "for the resub for", data.data.cumulativeMonths, "months!");
-        ChangeImage("static/img/pipwaddle.gif");
+        ChangeAlertImage("static/img/pipwaddle.gif");
         ShowAlert();
         PlayAudio("static/sound/catch-classic.mp3");
 
@@ -118,7 +146,7 @@ function StartSBotClient(host, port, auth) {
     
     client.on("Twitch.Cheer", (data) => {
         ChangeAlertText(null, data.data.user.name, "cheered", data.data.bits, "bits!");
-        ChangeImage("static/img/pipwai.png");
+        ChangeAlertImage("static/img/pipwai.png");
         ShowAlert();
         PlayAudio("static/sound/heal.mp3");
 
@@ -130,7 +158,7 @@ function StartSBotClient(host, port, auth) {
     client.on("Kofi.Donation", (data) => {
         console.log(data);
         ChangeAlertText(null, data.data.from, "donated", `${data.data.amount} ${data.data.currency}`, "via Ko-fi!");
-        ChangeImage("static/img/pipspin.gif");
+        ChangeAlertImage("static/img/pipspin.gif");
         ShowAlert();
         PlayAudio("static/sound/heal.mp3");
 
@@ -138,6 +166,29 @@ function StartSBotClient(host, port, auth) {
            HideAlert(); 
         }, 10000);
     });
+
+    client.on("Twitch.RewardRedemption", (data) => {
+        const rewardName = data.data.reward.title;
+
+        switch (rewardName) {
+            case "wacky waving inflatable arm flailing tube man":
+                const tubeImg = `static/img/tubeman${Math.floor(Math.random() * 5) + 1}.gif`;
+
+                ChangeRedeemImage(tubeImg);
+                break;
+            case "67":
+                ChangeRedeemImage("static/img/67.gif");
+                break;
+            default:
+                break;
+        }
+        
+        ShowRedeem(direction);
+
+        setTimeout(() => {
+            HideRedeem(direction);
+        }, 5000);
+    })
 
 
 
@@ -156,8 +207,13 @@ function Initialize() {
 
     // get authorization if any 
     let sbotPass = queryParams.get("pass") ?? "";
+    
+    // get alert location, if any
+    let alertPos = queryParams.get("alertPosition") ?? "top";
 
-    const client = StartSBotClient(sbotHost, sbotPort, sbotPass);
+    SetFlexDirection(alertPos);
+
+    const client = StartSBotClient(sbotHost, sbotPort, sbotPass, alertPos);
 }
 
 Initialize();
